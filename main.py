fc1beca1f3ee6e44871fdcde69697cc6c1c86042
zhangdavids <1269969739@qq.com>
2016-11-15 02:11:16 +0800

    add static resources

--- main.py
+++ main.py
@@ -1,47 +1,41 @@
 #! /usr/bin/env python2.7
 # coding=utf-8
 
-from bottle import route,run, request
+from bottle import route, run
+from bottle import template, static_file
+
+assets_path = './assets'
+
+# @route('/assets/<filename:re:.*\.png>')
+# def server_static(filename):
+#
+
+@route('/assets/<filename:re:.*\.css|.*\.js|.*\.png|.*\.jpg|.*\.gif>')
+def server_static(filename):
+    """定义/assets/下的静态(css,js,图片)资源路径"""
+    return static_file(filename, root=assets_path)
+
+
+@route('/assets/<filename:re:.*\.ttf|.*\.otf|.*\.eot|.*\.woff|.*\.svg|.*\.map>')
+def server_static(filename):
+    """定义/assets/字体资源路径"""
+    return static_file(filename, root=assets_path)
+
+
+@route('/')
+def index():
+    return template('index')
 
 @route('/login')
 def login():
-        '''
-        这里不指定方法时，默认就是GET方法
-        登陆页面，html代码都是直接从这里返回到网页中去的，如果不带任何方法，此函数将响应
-        关于模板的使用，后面课程会讲到
-        '''
-        return '''
-        <html>
-        <head>
-        </head>
-        <body>
-        <form action="/login" method="post">
-            Username: <input name="username" type="text" />
-            Password: <input name="password" type="password" />
-            <input value="Login" type="submit" />
-        </form>
-        </body>
-        </html>
-    '''
-
-@route('/login', method='POST')
-def do_login():
-        '''
-        函数名字随意定，这里是进行POST动作的，所以用了do_login，函数名不能与前后函数有同名
-        登陆动作，这里用了post，也就是当访问login页面，同时带了POST请求时，这个函数将响应
-        '''
-        username = request.forms.get('username')
-        password = request.forms.get('password')
-        return '<p>帐号:%s</p><p>密码:%s</p>' %(username,password)
-
+    return template('login')     #login是模板名，这里不需要填写后缀.tpl
 
 @route('/info')
 def info():
-    '''这里默认是GET方法，id,name,age将从url里获取，然后返回到网页内容中'''
-    id = request.query.id
-    name = request.query.name
-    age = request.query.age
-    return "id=%s,name=%s,age=%s" % (id, name, age)
-
+    name = 'tony'
+    age = '18'
+    qq = '12456809'
+    blog = 'www.github.com'
+    return template('info', tname=name, tage=age, tblog=blog,tqq=qq)
 
 run(host='0.0.0.0', port=8999, debug=True)
--- views/index.tpl
+++ views/index.tpl
@@ -1,5 +1,7 @@
-%rebase base title='python bottle 模板继承'
-<p>
-哈喽，我是首页，我这的代码很简单，就这么几行，但你要是在浏览器中打开源代码看看，</br>
-可就不止这几行了，而且包括了base.tpl和menu.tpl的所有代码，打开看看？
-</p>
+<html>
+    <head>
+    </head>
+    <body>
+    <p><img src="/assets/beauty.jpg"></img></p>
+    </body>
+</html>
